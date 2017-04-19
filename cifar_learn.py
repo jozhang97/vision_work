f28d079e55c82af433737267c37aa6e05ed7c78d
Jeffrey Zhang <jozhang@berkeley.eduX>
2017-04-18 19:58:08 -0700

    started conv

--- cifar_learn.py
+++ cifar_learn.py
@@ -3,36 +3,52 @@ import numpy as np
 from data_generator import Cifar
 cifar = Cifar()
 
-# HYPERPARAMETERS
+''' HYPERPARAMETERS '''
 n_classes = 10
-learning_rate = 0.1
-num_iter = 100
+learning_rate = 0.009
+num_iter = 1000
 batch_size = 100
-reg_coeff = 0.5
+reg_coeff = 0.05
 epsilon = tf.Variable(0.000000000000001 * np.ones([n_classes]), dtype=tf.float32)
+dropout_keep_prob = 0.5
 
-# DEFINE VARIABLES
+''' HELPER FUNCTION '''
+
+''' DEFINE VARIABLES '''
 x = tf.placeholder(tf.float32, [None, 3072])
+x_reshaped = tf.reshape(x, [-1, 32, 32, 3]) 
+filter_shape = [5,5,3,5]
+W_3 = tf.Variable(tf.truncated_normal(filter_shape, stddev = 0.1))
+b_3 = tf.Variable(tf.constant(0.1, shape=[5]))
+conv_3 = tf.nn.conv2d(x_reshaped, W_3, strides=[1,1,1,1], padding="SAME")
+relu_3 = tf.nn.relu(tf.nn.bias_add(conv_3, b_3))
+pooled_3 = tf.nn.max_pool(relu_3, ksize=[1,3,3,1], strides=[1,2,2,1], padding="SAME")
+y_3 = tf.reshape(pooled_3, [-1, 28*28*5]) 
+x_2 = y_3
+W_2 = tf.Variable(tf.random_uniform([3920, 1024], 0, 1, dtype=tf.float32, seed=0))
+b_2 = tf.Variable(tf.random_uniform([1024], 0, 1, dtype=tf.float32, seed=0))
+y_2 = tf.nn.relu(tf.matmul(x_2, W_2) + b_2)
+x_1 = y_2
+W_1 = tf.Variable(tf.random_uniform([1024, n_classes], 0, 1, dtype=tf.float32, seed=0))
+b_1 = tf.Variable(tf.random_uniform([n_classes], 0, 1, dtype=tf.float32, seed=0))
+y_1 = tf.matmul(x_1, W_1) + b_1
+# y_1 = tf.nn.dropout(y_1, dropout)
 y_true = tf.placeholder(tf.float32, [None, n_classes])
-W = tf.Variable(tf.random_uniform([3072, n_classes], 0, 1, dtype=tf.float32, seed=0))
-b = tf.Variable(tf.random_uniform([n_classes], 0, 1, dtype=tf.float32, seed=0))
-pre = tf.matmul(x,W) + b
-y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-# DEFINE LOSS FUNCTION
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_true * tf.log(y + epsilon), reduction_indices=[1]))
-mean_squared = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(y, y_true)))) 
-regularization = tf.nn.l2_loss(W) + tf.nn.l2_loss(b)
+''' DEFINE LOSS FUNCTION '''
+# try use this loss function tf.nn.log_poisson_loss
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_true * tf.log(y_1 + epsilon), reduction_indices=[1]))
+cross_entropy_2 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_1, labels=y_true))
+mean_squared = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(y_1, y_true)))) 
+regularization = tf.nn.l2_loss(W_1) + tf.nn.l2_loss(b_1) + tf.nn.l2_loss(W_2) + tf.nn.l2_loss(b_2)
+loss = reg_coeff * regularization + cross_entropy_2 
 
-#loss = reg_coeff * regularization + cross_entropy 
-loss = reg_coeff * regularization + mean_squared
+''' DEFINE OPTIMIZATION TECHNIQUE '''
+#train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
 
-# DEFINE OPTIMIZATION TECHNIQUE
-train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
-#train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
-
-
-# TRAIN
+''' TRAIN '''
+dropout_keep_prob = 0.5
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
@@ -40,16 +56,19 @@ for i in range(num_iter):
     batch_xs, batch_ys = cifar.train_next_batch(batch_size)
     sess.run(train_step, feed_dict={x: batch_xs, y_true:batch_ys})
 
-# TEST
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_true, 1)) #TODO: IS THIS THE RIGHT TESTING
+''' TEST '''
+dropout_keep_prob = 1
+correct_prediction = tf.equal(tf.argmax(y_1, 1), tf.argmax(y_true, 1)) #TODO: IS THIS THE RIGHT TESTING
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print(sess.run(accuracy, feed_dict={x: cifar.test_images, y_true: cifar.test_labels}))
 
 
-# DEBUG
-myY= sess.run(y, feed_dict={x: cifar.test_images, y_true: cifar.test_labels})
-myX = sess.run(x, feed_dict={x: cifar.test_images, y_true: cifar.test_labels})
-myW = sess.run(W, feed_dict={x: cifar.test_images, y_true: cifar.test_labels})
-myB = sess.run(b, feed_dict={x: cifar.test_images, y_true: cifar.test_labels})
-
-
+''' DEBUG '''
+y1= sess.run(y_1, feed_dict={x: cifar.test_images, y_true: cifar.test_labels})
+x1 = sess.run(x_1, feed_dict={x: cifar.test_images, y_true: cifar.test_labels})
+w1 = sess.run(W_1, feed_dict={x: cifar.test_images, y_true: cifar.test_labels})
+b1 = sess.run(b_1, feed_dict={x: cifar.test_images, y_true: cifar.test_labels})
+y2 = sess.run(y_2, feed_dict={x: cifar.test_images, y_true: cifar.test_labels})
+x2 = sess.run(x_2, feed_dict={x: cifar.test_images, y_true: cifar.test_labels})
+w2 = sess.run(W_2, feed_dict={x: cifar.test_images, y_true: cifar.test_labels})
+b2 = sess.run(b_2, feed_dict={x: cifar.test_images, y_true: cifar.test_labels})
--- data_generator.py
+++ data_generator.py
@@ -14,23 +14,53 @@ padding - pads the sides with 0's. keeps the size of the image the same
 import tensorflow as tf
 import random
 import numpy as np
+import time
 folder = "CIFAR_data/"
 class Cifar:
+
+# shuffle data and pick 100
+# randomly pick 100
     def __init__(self):
         s1 = unpickle(folder+"data_batch_1")
         s2 = unpickle(folder+"data_batch_2")
         s3 = unpickle(folder+"data_batch_3")
         s4 = unpickle(folder+"data_batch_4")
         s5 = unpickle(folder+"data_batch_5")
-        self.s = [s1,s2,s3,s4,s5]
-        st = unpickle(folder+"test_batch")
-        self.test_images = st['data']/255.0
-        self.test_labels = one_hot(st['labels'])
+        start = time.time()
+        s1 = zip(s1['data'], s1['labels'])
+        s2 = zip(s2['data'], s2['labels'])
+        s3 = zip(s3['data'], s3['labels'])
+        s4 = zip(s4['data'], s4['labels'])
+        #s5 = zip(s5['data'], s5['labels'])
+        s1.extend(s2)
+        s1.extend(s3)
+        s1.extend(s4)
+        #s1.extend(s5)
+        random.shuffle(s1)
+        elapsedTime = time.time() - start # t = 0.04
+        self.train_data = s1
+
+        self.test_images = s5['data']/255.0
+        self.test_labels = one_hot(s5['labels'])
+        #st = unpickle(folder+"test_batch")
+        #self.test_images = st['data']/255.0
+        #self.test_labels = one_hot(st['labels'])
      
     def train_next_batch(self, batch_size):
-        index = int(random.random() * 5)  
-        # TODO: put in batch_size
-        return self.s[index]['data']/255.0, one_hot(self.s[index]['labels'])
+        start = time.time()
+        train_data = self.train_data
+        n = len(train_data)
+        data = []; labels = []; picked = []
+        for _ in range(batch_size):
+            index = random.randint(0, n - 1)
+            while (index in picked):
+                index = random.randint(0, n - 1)
+            data.append(train_data[index][0])
+            labels.append(train_data[index][1])
+            picked.append(index)
+        elapsedTime = time.time() - start # t = 0.0004
+        return np.array(data)/255.0, one_hot(labels) 
+        #return self.s[index]['data']/255.0, one_hot(self.s[index]['labels'])
  
 def one_hot(lst): 
     # lst: a list of labels
