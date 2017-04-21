152895818bba7569bfc327145bbca82353898a64
JEFFREY ZHANG <jozhang@berkeley.edu>
2017-04-20 17:14:35 -0700

    Added learning rate drop, variable summaries, and histograms
    Added another conv layer, removed regularization, make learning rate drop faster

--- mnist_powerful.py
+++ mnist_powerful.py
@@ -7,8 +7,9 @@ cifar= input_data.read_data_sets("MNIST_data/", one_hot=True)
 n_classes = 10
 learning_rate = 0.1
 learning_rate_decay = 0.1
-num_epochs_per_decay = 350
-num_iter = 100
+decays_per_epoch= 1/10
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 60000 
+
 batch_size = 100 
 reg_coeff = 0.0005
 epsilon = tf.Variable(0.000000000000001 * np.ones([n_classes]), dtype=tf.float32)
@@ -40,27 +41,33 @@ conv_4 = tf.nn.conv2d(x_reshaped, W_4, strides=[1,1,1,1], padding="VALID")
 relu_4 = tf.nn.relu(tf.nn.bias_add(conv_4, b_4))
 pooled_4 = tf.nn.max_pool(relu_4, ksize=[1,3,3,1], strides=[1,1,1,1], padding="VALID")
 y_4 = tf.nn.local_response_normalization(pooled_4)
-'''
 filter_shape = [5,5,64,64]
 W_3 = tf.Variable(tf.random_uniform(filter_shape))
+variable_summaries(W_3)
 b_3 = tf.Variable(tf.random_uniform([64]))
+variable_summaries(b_3)
 conv_3 = tf.nn.conv2d(y_4, W_3, strides=[1,1,1,1], padding="VALID")
 conv_3 = tf.nn.local_response_normalization(conv_3)
 relu_3 = tf.nn.relu(tf.nn.bias_add(conv_3, b_3))
 pooled_3 = tf.nn.max_pool(relu_3, ksize=[1,3,3,1], strides=[1,1,1,1], padding="VALID")
 y_3 = tf.reshape(pooled_3, [-1, 16*16*64])
-'''
-x_22 = tf.reshape(y_4, [-1, 22*22*64])
-W_22 = tf.Variable(tf.random_uniform([22*22*64,512], 0, 100, dtype=tf.float32, seed=0))
+x_22 = tf.reshape(y_4, [-1, 16*16*64])
+W_22 = tf.Variable(tf.random_uniform([16*16*64,512], 0, 100, dtype=tf.float32, seed=0))
+variable_summaries(W_22)
 b_22 = tf.Variable(tf.random_uniform([512], 0, 1, dtype=tf.float32, seed=0))
+variable_summaries(b_22)
 y_22 = tf.nn.relu(tf.nn.bias_add(tf.matmul(x_22, W_22), b_22))
 x_2 = y_22
 W_2 = tf.Variable(tf.random_uniform([512, 256], 0, 100, dtype=tf.float32, seed=0))
+variable_summaries(W_2)
 b_2 = tf.Variable(tf.random_uniform([256], 0, 1, dtype=tf.float32, seed=0))
+variable_summaries(b_2)
 y_2 = tf.nn.relu(tf.nn.bias_add(tf.matmul(x_2, W_2), b_2))
 x_1 = y_2
 W_1 = tf.Variable(tf.random_uniform([256, n_classes], 0, 1, dtype=tf.float32, seed=0))
+variable_summaries(W_1)
 b_1 = tf.Variable(tf.random_uniform([n_classes], 0, 1, dtype=tf.float32, seed=0))
+variable_summaries(b_1)
 y_1_eval = tf.nn.bias_add(tf.matmul(x_1, W_1) , b_1)
 y_1 = tf.nn.dropout(y_1_eval, dropout_keep_prob)
 y_true = tf.placeholder(tf.float32, [None, n_classes])
@@ -78,10 +85,11 @@ g = lambda alpha,x_s, y_s: sess.run(alpha, feed_dict={x: x_s, y_true: y_s})
 cross_entropy_2 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_1, labels=y_true))
 tf.summary.scalar('cross_entropy', cross_entropy_2)
 regularization = tf.nn.l2_loss(W_1) + tf.nn.l2_loss(b_1) + tf.nn.l2_loss(W_2) + tf.nn.l2_loss(b_2) + tf.nn.l2_loss(W_22) + tf.nn.l2_loss(b_22) + tf.nn.l2_loss(W_4) + tf.nn.l2_loss(b_4)
-loss = cross_entropy_2 + regularization * reg_coeff
+loss = cross_entropy_2 
 
 
 ''' DEFINE OPTIMIZATION TECHNIQUE '''
+tf.summary.scalar('learning_rate', learning_rate)
 train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
 
 correct_prediction = tf.equal(tf.argmax(y_1_eval, 1), tf.argmax(y_true, 1)) #TODO: IS THIS THE RIGHT TESTING
@@ -91,20 +99,25 @@ tf.summary.scalar('accuracy', accuracy)
 ''' TRAIN '''
 
 sess = tf.Session()
-
 tf.summary.scalar('training_loss', loss)
-tf.summary.histogram('histogram', loss)
+tf.summary.histogram('Training_loss', loss) 
+tf.summary.histogram('Accuracy', accuracy)
+
+
+
+
 merged = tf.summary.merge_all()
 
 init = tf.global_variables_initializer()
-train_writer = tf.summary.FileWriter('tensorboard_log/train', sess.graph)
-test_writer = tf.summary.FileWriter('tensorboard_log/test')
+train_writer = tf.summary.FileWriter('tensorboard_log2/train', sess.graph)
+test_writer = tf.summary.FileWriter('tensorboard_log2/test')
 sess.run(init)
-for i in range(num_iter):
+for i in range(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN * 10):
     batch_xs, batch_ys = cifar.train.next_batch(batch_size)
     if i % 10 == 0:
-        summary,loss = sess.run([merged, loss], feed_dict={x:cifar.test.images, y_true: cifar.test.labels})
+        summary,acc= sess.run([merged, accuracy], feed_dict={x:cifar.test.images, y_true: cifar.test.labels})
         test_writer.add_summary(summary, i)
+        print(acc)
     else:
         summary,_ = sess.run([merged, train_step], feed_dict={x: batch_xs, y_true:batch_ys})
         train_writer.add_summary(summary, i)
@@ -112,8 +125,8 @@ for i in range(num_iter):
         #print(f(loss))
         #print(sess.run(accuracy, feed_dict={x: batch_xs, y_true: batch_ys}))
         #print(sess.run(accuracy, feed_dict={x: cifar.test.images[:100], y_true: cifar.test.labels[:100]}))
-    if i%1000 == 0:
-        learning_rate *= 0.5
+    if i%NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN * decays_per_epoch == 0:
+        learning_rate *= learning_rate_decay 
 
 
 
