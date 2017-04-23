8836db1b4fbc0c6a87cc0aa94b6465df11df4749
JEFFREY ZHANG <jozhang@berkeley.edu>
2017-04-23 08:13:01 -0700

    Updated net to look like tutorial MNIST

--- mnist_powerful.py
+++ mnist_powerful.py
@@ -15,7 +15,7 @@ NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 60000
 batch_size = 100 
 reg_coeff = 0.00005
 epsilon = tf.Variable(0.000000000000001 * np.ones([n_classes]), dtype=tf.float32)
-dropout_keep_prob = 0.5
+dropout_keep_prob = tf.placeholder(tf.float32)
 
 ''' HELPER FUNCTION '''
 def variable_summaries(var):
@@ -30,54 +30,54 @@ def variable_summaries(var):
     tf.summary.scalar('min', tf.reduce_min(var))
     tf.summary.histogram('histogram', var)
 
+def variable_summaries_map(mapp):
+    for vals in mapp.values():
+        variable_summaries(vals)
 
-''' DEFINE VARIABLES '''
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='SAME')
+
 with tf.device(device_name):
+    ''' DEFINE VARIABLES '''
+    W = {"W_1": weight_variable([1024, n_classes]),
+        "W_2": weight_variable([7*7*64, 1024]), 
+        "W_3": weight_variable([5, 5, 32, 64]),
+        "W_4": weight_variable([5, 5, 1, 32]), 
+        }
+    b = {"b_1": bias_variable([n_classes])
+        "b_2": bias_variable([1024])
+        "b_3": bias_variable([64])
+        "b_4": bias_variable([32])
+        }
+    variable_summaries_map(W)
+    variable_summaries_map(b)
+
     x = tf.placeholder(tf.float32, [None, 784])
     x_reshaped = tf.reshape(x, [-1, 28, 28, 1]) 
 
-    W_4 = tf.Variable(tf.truncated_normal([5,5,1,64], mean=0, stddev=1.0))
-    variable_summaries(W_4)
-    b_4 = tf.Variable(tf.truncated_normal([64], mean=0, stddev=1.0))
-    variable_summaries(b_4)
-    conv_4 = tf.nn.conv2d(x_reshaped, W_4, strides=[1,1,1,1], padding="VALID")
-    relu_4 = tf.nn.relu(tf.nn.bias_add(conv_4, b_4))
-    pooled_4 = tf.nn.max_pool(relu_4, ksize=[1,3,3,1], strides=[1,1,1,1], padding="VALID")
-    y_4 = tf.nn.local_response_normalization(pooled_4)
-    filter_shape = [5,5,64,64]
-    W_3 = tf.Variable(tf.truncated_normal(filter_shape, mean=0, stddev=1.0))
-    variable_summaries(W_3)
-    b_3 = tf.Variable(tf.truncated_normal([64], mean=0, stddev=1.0))
-    variable_summaries(b_3)
-    conv_3 = tf.nn.conv2d(y_4, W_3, strides=[1,1,1,1], padding="VALID")
-    conv_3 = tf.nn.local_response_normalization(conv_3)
-    relu_3 = tf.nn.relu(tf.nn.bias_add(conv_3, b_3))
-    pooled_3 = tf.nn.max_pool(relu_3, ksize=[1,3,3,1], strides=[1,1,1,1], padding="VALID")
-    y_3 = tf.reshape(pooled_3, [-1, 16*16*64])
-    x_22 = tf.reshape(y_3, [-1, 16*16*64])
-    W_22 = tf.Variable(tf.truncated_normal([16*16*64,512], mean=0, stddev=1.0))
-    variable_summaries(W_22)
-    b_22 = tf.Variable(tf.truncated_normal([512], mean=0, stddev=1.0))
-    variable_summaries(b_22)
-    y_22 = tf.nn.relu(tf.nn.bias_add(tf.matmul(x_22, W_22), b_22))
-    x_2 = y_22
-    W_2 = tf.Variable(tf.truncated_normal([512, 256], mean=0, stddev=1.0))
-    variable_summaries(W_2)
-    b_2 = tf.Variable(tf.truncated_normal([256], mean=0, stddev=1.0))
-    variable_summaries(b_2)
-    y_2 = tf.nn.relu(tf.nn.bias_add(tf.matmul(x_2, W_2), b_2))
-    x_1 = y_2
-    W_1 = tf.Variable(tf.truncated_normal([256, n_classes], mean=0, stddev=1.0))
-    variable_summaries(W_1)
-    b_1 = tf.Variable(tf.truncated_normal([n_classes], mean=0, stddev=1.0))
-    variable_summaries(b_1)
-    y_1_eval = tf.nn.bias_add(tf.matmul(x_1, W_1) , b_1)
-    y_1 = tf.nn.dropout(y_1_eval, dropout_keep_prob)
+    y_4 = max_pool_2x2(tf.nn.relu(tf.nn.bias_add(conv2d(x_reshaped, W["W_4"]), b["b_4"])))
+    # y_4 = tf.nn.local_response_normalization(y_4)
+
+    y_3 = max_pool_2x2(tf.nn.relu(tf.nn.bias_ass(conv2d(y_4, W["W_3"]), b["b_3"])))
+    # y_3 = tf.nn.max_pool(tf.nn.local_response_normalization(tf.nn.relu(tf.nn.bias_ass(conv2d(y_4, W["W_3"]), b["b_3"]))))
+    y_3 = tf.reshape(y_3, [-1, 7*7*64])
+
+    y_2 = tf.nn.relu(tf.nn.bias_add(tf.matmul(y_3, W["W_2"]), b["b_2"]))
+    y_2 = tf.nn.dropout(y_2, dropout_keep_prob)
+    y_1 = tf.nn.bias_add(tf.matmul(y_2, W["W_1"]) , b["b_1"])
     y_true = tf.placeholder(tf.float32, [None, n_classes])
-
-    f = lambda alpha: sess.run(alpha, feed_dict={x: cifar.test.images, y_true: cifar.test.labels})
-    g = lambda alpha,x_s, y_s: sess.run(alpha, feed_dict={x: x_s, y_true: y_s})
-
 
     # # LOSS FUNCTIONS
     # with tf.variable_scope('weights_norm') as scope:
@@ -95,10 +95,11 @@ with tf.device(device_name):
     # tf.add_to_collection('losses', cross_entropy)
     # loss = tf.add_n(tf.get_collection('losses'), name='total_loss')
 
-    reg_coeff = 0.01
-    regularization = tf.nn.l2_loss(W_1) + tf.nn.l2_loss(b_1) + tf.nn.l2_loss(W_2) + tf.nn.l2_loss(b_2) + tf.nn.l2_loss(W_22) + tf.nn.l2_loss(b_22) + tf.nn.l2_loss(W_4) + tf.nn.l2_loss(b_4) + tf.nn.l2_loss(W_3) + tf.nn.l2_loss(b_3)
-    tf.summary.scalar("regularization", regularization)
-    loss = cross_entropy + reg_coeff * regularization
+    # reg_coeff = 0.01
+    # regularization = tf.nn.l2_loss(W_1) + tf.nn.l2_loss(b_1) + tf.nn.l2_loss(W_2) + tf.nn.l2_loss(b_2) + tf.nn.l2_loss(W_22) + tf.nn.l2_loss(b_22) + tf.nn.l2_loss(W_4) + tf.nn.l2_loss(b_4) + tf.nn.l2_loss(W_3) + tf.nn.l2_loss(b_3)
+    # tf.summary.scalar("regularization", regularization)
+    # loss = cross_entropy + reg_coeff * regularization
+    loss = cross_entropy
     tf.summary.scalar('loss', loss)
     tf.summary.histogram('loss', loss) 
 
@@ -106,7 +107,7 @@ with tf.device(device_name):
 
     ''' DEFINE OPTIMIZATION TECHNIQUE '''
     train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
-    correct_prediction = tf.equal(tf.argmax(y_1_eval, 1), tf.argmax(y_true, 1)) 
+    correct_prediction = tf.equal(tf.argmax(y_1, 1), tf.argmax(y_true, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     tf.summary.scalar('accuracy', accuracy)
     tf.summary.histogram('accuracy', accuracy)
@@ -120,22 +121,18 @@ sess = tf.Session(config = config)
 #sess = tf.Session()
 merged = tf.summary.merge_all()
 init = tf.global_variables_initializer()
-train_writer = tf.summary.FileWriter('tensorboard_log7/train', sess.graph)
-test_writer = tf.summary.FileWriter('tensorboard_log7/test')
+train_writer = tf.summary.FileWriter('tensorboard_log_mnist/train', sess.graph)
+test_writer = tf.summary.FileWriter('tensorboard_log_mnist/test')
 sess.run(init)
 for i in range(60000* 10):
     batch_xs, batch_ys = cifar.train.next_batch(batch_size)
-    if i % 10 == 0:
-        summary,acc= sess.run([merged, accuracy], feed_dict={x:cifar.test.images, y_true: cifar.test.labels})
+    if i % 100 == 0:
+        summary,acc= sess.run([merged, accuracy], feed_dict={x:cifar.test.images, y_true: cifar.test.labels, dropout_keep_prob: 1})
         test_writer.add_summary(summary, i)
         print(acc)
     else:
-        summary,_ = sess.run([merged, train_step], feed_dict={x: batch_xs, y_true:batch_ys})
+        summary,_ = sess.run([merged, train_step], feed_dict={x: batch_xs, y_true:batch_ys, dropout_keep_prob: 0.5})
         train_writer.add_summary(summary, i)
-    #if i%100== 0:
-        #print(f(loss))
-        #print(sess.run(accuracy, feed_dict={x: batch_xs, y_true: batch_ys}))
-        #print(sess.run(accuracy, feed_dict={x: cifar.test.images[:100], y_true: cifar.test.labels[:100]}))
     if i % 6000 == 0 and i > 0:
     #if i%NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN * decays_per_epoch == 0:
         learning_rate *= learning_rate_decay 
